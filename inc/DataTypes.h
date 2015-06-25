c983a75c91679e955f2725ad04afa89ddccdcaf2
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-06-25 15:22:54 -0700

    Remove PinDriver and revise DataTypes

--- inc/DataTypes.h
+++ inc/DataTypes.h
@@ -16,29 +16,4 @@ typedef enum
 #define RETURN_IF_NULL(pointer) if ((pointer) == NULL) return
 #define RETURN_IF_NULL_RETURN_VALUE(pointer, retVal) if ((pointer) == NULL) return (retVal)
 
-
-//Function return values
-typedef enum
-{
-  PIN_NULL_POINTER = -66,
-  PIN_UNDEFINED = -1,
-  PIN_LOW = 0,
-  PIN_HIGH  = 1
-} PinState;
-
-#define NULL_POINTER 66
-
-
-typedef struct PinStruct * Pin;
-
-//This data type is designed for pins that will be in existence for the duration
-//of the program!
-Pin Pin_Create(uint8_t * address, uint8_t bitmask);
-void Pin_Destroy(Pin * self);
-
-uint8_t * Pin_GetAddress(Pin self);
-uint8_t Pin_GetBitmask(Pin self);
-PinState Pin_GetState(Pin self);
-void Pin_SetState(Pin self, PinState state);
-
 #endif
